885b5df18bdd17e088c40d76e1e2caff3b898533
Ankit <45582571+Ankit-Xs@users.noreply.github.com>
2020-05-02 19:44:34 +0530

    Update csgo-bhop-script.py

--- csgo-bhop-script.py
+++ csgo-bhop-script.py
@@ -1,23 +1,33 @@
-#Made by Zen
-
 import keyboard
 import winsound
 import time
-          
+import tkinter
+from tkinter import *
+
+window = Tk()
+
+window.title("CS:GO Bhop-Script Python")
+
+window.geometry('350x200')
+
+lbl = Label(window, text="Click here to Start -->")
+
+lbl.grid(column=0, row=0)
+
 def bhop():
-    print("AutoBhop has launched")
-    winsound.Beep(1000, 200)
     while True:
         if keyboard.is_pressed('space'):
             keyboard.press_and_release('space')
-            time.sleep(0.09)
-        else:
-            continue
+            time.sleep(0.05)
 
-bhop()
-        
-            
+def clicked():
+    winsound.Beep(1000, 200)
+    bhop()
+23
+btn = Button(window, text="UwU" , command = clicked)
 
+btn.grid(column=1, row=0)
+window.mainloop()
 
         
 
